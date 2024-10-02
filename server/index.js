31ed22561fe3fc097bce3ce66ce140b229994ccd
Cristian Canapi <cristiancanapi730@gmail.com>
2024-10-01 20:50:35 -0400

    Backend en index.js de mascotas

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+// Conexion con la BD
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -14,6 +15,7 @@ const db = mysql.createConnection({
     port: 3305,
 });
 
+// BACKEND USUARIOS
 app.post("/create", (req, res) => {
     console.log(req.body);
     const nombre = req.body.nombre;
@@ -77,6 +79,74 @@ app.delete("/delete/:id", (req, res) => {
             if (err) {
                 console.log(err);
                 return res.status(500).send("Error al eliminar el usuario");
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+// BACKEND MASCOTAS
+
+// Crear una mascota
+app.post("/createMascota", (req, res) => {
+    const { nombre, especie, edad, id_usuario } = req.body;
+
+    db.query(
+        'INSERT INTO mascotas(nombre, especie, edad, id_usuario) VALUES(?,?,?,?)',
+        [nombre, especie, edad, id_usuario],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send("Error al registrar la mascota");
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+// Obtener todas las mascotas
+app.get("/mascotas", (req, res) => {
+    db.query('SELECT * FROM mascotas',
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send("Error al cargar las mascotas");
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+// Actualizar una mascota
+app.put("/updateMascota", (req, res) => {
+    const { nombre, especie, edad, id_usuario, id } = req.body;
+
+    db.query(
+        'UPDATE mascotas SET nombre=?, especie=?, edad=?, id_usuario=? WHERE id=?',
+        [nombre, especie, edad, id_usuario, id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send("Error al actualizar la mascota");
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+// Eliminar una mascota
+app.delete("/deleteMascota/:id", (req, res) => {
+    const id = req.params.id;
+    db.query(
+        'DELETE FROM mascotas WHERE id=?', [id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send("Error al eliminar la mascota");
             } else {
                 res.send(result);
             }
